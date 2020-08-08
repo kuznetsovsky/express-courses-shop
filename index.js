c1a9e20b632ae756f78c4b46418b129dda52a323
Dmitriy Kuznetsov <kuznetsovsky@inbox.ru>
2020-08-08 17:25:58 +0300

    Activates the static folder express

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require(`express`);
 const exphbs = require(`express-handlebars`);
-
 const app = express();
 
 const hbs = exphbs.create({
@@ -11,6 +10,8 @@ const hbs = exphbs.create({
 app.engine(`hbs`, hbs.engine);
 app.set(`view engine`, `hbs`);
 app.set(`views`, `views`);
+
+app.use(express.static(`public`));
 
 app.get(`/`, (req, res) => {
   res.render(`main`);
