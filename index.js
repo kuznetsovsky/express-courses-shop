aca4e2630c266be9136a4eb908040643ab041875
Dmitriy Kuznetsov <kuznetsovsky@inbox.ru>
2020-08-08 16:25:15 +0300

    Adds routes

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
+const path = require(`path`);
 const express = require(`express`);
 const app = express();
+
+app.get(`/`, (req, res) => {
+  res.sendFile(path.join(__dirname, `views`, `main.html`));
+});
+
+app.get(`/about`, (req, res) => {
+  res.sendFile(path.join(__dirname, `views`, `about.html`));
+});
+
 
 const PORT = process.env.PORT || 3000;
 
