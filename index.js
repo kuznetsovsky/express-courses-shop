94082831630854f87986955813845b7b4a9096e1
Dmitriy Kuznetsov <kuznetsovsky@inbox.ru>
2020-08-08 16:48:27 +0300

    Configures handlebars

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
-const path = require(`path`);
 const express = require(`express`);
+const exphbs = require(`express-handlebars`);
+
 const app = express();
 
+const hbs = exphbs.create({
+  defaultLayout: `main`,
+  extname: `hbs`,
+});
+
+app.engine(`hbs`, hbs.engine);
+app.set(`view engine`, `hbs`);
+app.set(`views`, `views`);
+
 app.get(`/`, (req, res) => {
-  res.sendFile(path.join(__dirname, `views`, `main.html`));
+  res.render(`main`);
 });
 
 app.get(`/about`, (req, res) => {
-  res.sendFile(path.join(__dirname, `views`, `about.html`));
+  res.render(`about`);
 });
-
 
 const PORT = process.env.PORT || 3000;
 
