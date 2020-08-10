6cc0c5fff033fbd43ef3cc8c7c8fb3217689fd99
Dmitriy Kuznetsov <kuznetsovsky@inbox.ru>
2020-08-10 12:01:18 +0300

    Adds redrawing of items in the cart

--- public/app.js
+++ public/app.js
@@ -1,21 +1,45 @@
-document.querySelectorAll(`.price`).forEach((it) => {
-  it.textContent = new Intl.NumberFormat(`ru-RU`, {
+const cardNode = document.querySelector(`#card`);
+
+const toCurrency = (price) => {
+  return new Intl.NumberFormat(`ru-RU`, {
     currency: `rub`,
     style: `currency`,
-  }).format(it.textContent);
-});
+  }).format(price);
+}
 
-const cardNode = document.querySelector(`#card`);
+document.querySelectorAll(`.price`).forEach((it) => {
+  it.textContent = toCurrency(node.textContent);
+});
 
 if (cardNode) {
   cardNode.addEventListener(`click`, (event) => {
     if (event.target.classList.contains(`js-course-remove`)) {
       const id = event.target.dataset.courseId;
-      
+
       fetch(`/card/remove/${id}`, {
         method: `delete`
-      }).then((res) => res.json)
-        .then((card) => console.log(card))
+      }).then((res) => res.json())
+        .then((card) => {
+          if (card.courses.length) {
+            const html = card.courses.map((it) => {
+              return `
+                <tr>
+                  <td>${it.title}</td>
+                  <td>${it.count}</td>
+                  <td>
+                    <button class="waves-effect waves-light btn-small js-course-remove" data-course-id="${it._id}">Delete</button>
+                  </td>
+                </tr>
+              `;
+            }).join(``);
+
+            cardNode.querySelector(`tbody`).innerHTML = html;
+            cardNode.querySelector(`.price`).textContent = toCurrency(card.totalPrice);
+
+          } else {
+            cardNode.innerHTML = `<p>There are no items in your cart.</p>`;
+          }
+        })
     }
   });
 }
