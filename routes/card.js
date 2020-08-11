f5abde128a173ab029d33554a557b78922965d10
Dmitriy Kuznetsov <kuznetsovsky@inbox.ru>
2020-08-11 11:27:21 +0300

    Fixes the display of products in the cart

--- routes/card.js
+++ routes/card.js
@@ -1,21 +1,33 @@
-const {Router} = require(`express`);
+const { Router } = require(`express`);
 const router = Router();
 
 const Course = require(`../models/course`);
 
 router.get(`/`, async (req, res) => {
-  const card = await Card.fetch();
+  const user = await req.user.
+    populate(`cart.items.courseId`).
+    execPopulate();
+
+  const courses = user.cart.items.map((it) => ({
+    ...it.courseId._doc,
+    count: it.count
+  }));
+
+  const totalPrice = courses.reduce((total, it) => {
+    return total += it.price * it.count;
+  }, 0);
+
   res.render(`card`, {
     title: `Basket of goods`,
-    courses: card.courses,
-    totalPrice: card.totalPrice,
+    courses,
+    totalPrice,
   });
 });
 
 router.post(`/add`, async (req, res) => {
   const course = await Course.findById(req.body._id);
   await req.user.addToCart(course);
-  res.redirect(`/card`);  
+  res.redirect(`/card`);
 });
 
 router.delete(`/remove/:id`, async (req, res) => {
