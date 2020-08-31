b6f38099989c70fcc2220b273a3c5f6d9920150d
Dmitriy Kuznetsov <kuznetsovsky@inbox.ru>
2020-08-31 14:58:59 +0300

    Removes development data

--- keys/keys.dev.js
+++ keys/keys.dev.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB_URI: `mongodb+srv://comebas:gLlBIWOQqHqHC770@cluster0.gl3ie.mongodb.net/courseshop`,
-  SESSION_SECRET: `some secret value`,
+  MONGODB_URI: ``,
+  SESSION_SECRET: ``,
   BASE_URL: `http://localhost:3000`,
 };
